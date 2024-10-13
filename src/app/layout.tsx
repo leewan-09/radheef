3875a7e8a767cae3a812245549ad70bbf96f283a
Abdulla Zihaan <abozihaan@gmail.com>
2024-10-13 21:45:25 +0500

    Resolved conflict in layout.tsx to keep bg-white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,16 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body className="bg-muted">
+<<<<<<< HEAD
+      <body className="bg-white">
+      <Script
+        defer
+        src="https://plausible.boxmv.org/js/script.js"
+        data-domain="radheef.xyz"
+      />
+=======
+
+>>>>>>> origin/main
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
